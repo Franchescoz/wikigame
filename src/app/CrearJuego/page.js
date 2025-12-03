7bd1dbc86f87480133874e5c0e235bd1f3ffaf05
Francisco <fcorrom2907@g.educaand.es>
2025-12-03 09:10:13 +0100

    Version 2.0

--- src/app/CrearJuego/page.js
+++ src/app/CrearJuego/page.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { Navbar } from "./layout";
+import CrearVistaJuego from "./CrearJuego";
 
 export default function Home() {
   return (
     <div >
       <Navbar/>
-    
+    <CrearVistaJuego/>
     <Footer/>
     </div>
   );
