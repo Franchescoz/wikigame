e8a7c9087a0f52e10a43384d338a0d9414d60529
Francisco <fcorrom2907@g.educaand.es>
2025-12-03 09:04:12 +0100

    Version 2.0

--- src/app/VistaJuego/page.js
+++ src/app/VistaJuego/page.js
@@ -1,13 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Perfil from "./Perfil/perfil";
+import VistaJuego from "./VistaJuego";
 import { Navbar } from "./layout";
+import { Footer } from "../layout";
 
 export default function Home() {
   return (
     <div >
       <Navbar/>
+     <VistaJuego/>
+     <Footer/>
     
-    <Footer/>
     </div>
   );
 }
